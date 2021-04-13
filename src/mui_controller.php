3bc1fa45a78eea6579914bdfe523be73fa6e1653
陈庙琴 <1421224516@qq.com>
2021-04-13 16:41:00 +0800

    Initial commit

---
+++ src/mui_controller.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 描述：
+ * Created at 2021/3/15 19:30 by 陈庙琴
+ */
+
+namespace oba_mui_style;
+
+
+use epii\app\controller;
+use oba_mui_style\libs\trait_set\MuiTrait;
+
+class mui_controller extends controller
+{
+    use MuiTrait;
+}
